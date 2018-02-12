2d6a0604b1584525e08209ed8deb2c3734c17aea
Shih Wei <loneswei@gmail.com>
2018-02-12 17:37:32 +0800

    Used back the previous grid system, added gaps
    Sorry for the reverting stupidity :anguished:

--- Studio_Project_04/Assets/GridSystem.cs
+++ Studio_Project_04/Assets/GridSystem.cs
@@ -2,26 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridSystem : MonoBehaviour {
-
-
-	public GameObject Ground;
-	public int Width = 10;
-	public int Height = 10;
+public class GridSystem : MonoBehaviour
+{
+	// Serializable private variables defining Grid
+	[SerializeField]
+	GameObject Ground;
+	[SerializeField]
+	int Rows = 10;
+	[SerializeField]
+	int Columns = 10;
 
 	private GameObject[,] Grid;
 
-	// Use this for initialization
 	void Awake()
 	{
-		Grid = new GameObject[Width, Height];
-		for (int x = 0; x < Width; ++x)
+		// Create Grid based on inputted number of rows and columns
+		Grid = new GameObject[Rows, Columns];
+
+		for (int x = 0; x < Rows; ++x)
 		{
-			for (int z = 0; z < Height; ++z)
+			for (int z = 0; z < Columns; ++z)
 			{
+				// Create a Ground
 				GameObject GridGround = (GameObject)Instantiate (Ground);
-				GridGround.transform.position = new Vector3 (GridGround.transform.position.x + x,
-					GridGround.transform.position.y, GridGround.transform.position.z + z);
+
+				// Set the position according to Grid
+				// (0.2f * xz) - this determines the size of gap in between the Grounds
+				GridGround.transform.position = new Vector3 (GridGround.transform.position.x + x + (0.2f * x),
+					GridGround.transform.position.y, GridGround.transform.position.z + z + (0.2f * z));
 				Grid [x, z] = GridGround;
 			}
 		}
