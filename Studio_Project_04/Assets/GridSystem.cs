9ede6a98de09fcde2c2176cd65ef31c9e979f193
Shih Wei <loneswei@gmail.com>
2018-02-12 15:21:05 +0800

    Grid system
    Need to click on play to generate the grounds.

---
+++ Studio_Project_04/Assets/GridSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem : MonoBehaviour {
+
+
+	public GameObject Ground;
+	public int Width = 10;
+	public int Height = 10;
+
+	private GameObject[,] Grid;
+
+	// Use this for initialization
+	void Awake()
+	{
+		Grid = new GameObject[Width, Height];
+		for (int x = 0; x < Width; ++x)
+		{
+			for (int z = 0; z < Height; ++z)
+			{
+				GameObject GridGround = (GameObject)Instantiate (Ground);
+				GridGround.transform.position = new Vector3 (GridGround.transform.position.x + x,
+					GridGround.transform.position.y, GridGround.transform.position.z + z);
+				Grid [x, z] = GridGround;
+			}
+		}
+
+	}
+}
