9ffe87c6eb481b3e293c6e015df3d02400f152f6
Ai Long <lucifer967@hotmail.com>
2018-02-19 09:18:08 +0800

    Beautified the main menu slightly.

---
+++ Studio_Project_04/Assets/Scripts/UI Nonsense Scripts/FillingUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillingUI : MonoBehaviour {
+
+	[SerializeField]
+	float FillAmount;
+
+	[SerializeField]
+	float FillRate;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GetComponent<Image> ().fillAmount < FillAmount) {
+			GetComponent<Image> ().fillAmount += FillRate;
+		}
+	}
+}
