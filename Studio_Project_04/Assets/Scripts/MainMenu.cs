7c71503d7eb65d21e3ff2c8c67d1b9a3045a3a2a
Shih Wei <loneswei@gmail.com>
2018-03-01 16:39:36 +0800

    Added Loading image to sceneMenu
    Can load SceneOpen in background with loading image shown

--- Studio_Project_04/Assets/Scripts/MainMenu.cs
+++ Studio_Project_04/Assets/Scripts/MainMenu.cs
@@ -12,11 +12,13 @@ public class MainMenu : MonoBehaviour
 	Color FadeIn_Color;
 
 	bool Btn_Pressed = false;
+	bool LoadedOnce = false;
 
 	string SceneName;
 
 	void Start()
 	{
+		FadeIn_Color = FadeIn.GetComponent<Image> ().color;
 		FadeIn_Color.a = 1;
 	}
 
@@ -34,9 +36,20 @@ public class MainMenu : MonoBehaviour
 			FadeIn_Color.a = 0;
 		}
 
-		if (FadeIn_Color.a >= 1 && Btn_Pressed == true) {
-			SceneManager.LoadScene (SceneName);
+		if (FadeIn_Color.a >= 1 && Btn_Pressed == true && !LoadedOnce) {
+			StartCoroutine (LoadAsyncScene ());
+			LoadedOnce = true;
 		}
+	}
+
+	IEnumerator LoadAsyncScene()
+	{
+		AsyncOperation asynLoad = SceneManager.LoadSceneAsync (SceneName);
+		asynLoad.allowSceneActivation = false;
+		while (asynLoad.progress < 0.9f)
+			yield return null;
+		
+		asynLoad.allowSceneActivation = true;
 	}
 
 	public void Pressed(string Works)
